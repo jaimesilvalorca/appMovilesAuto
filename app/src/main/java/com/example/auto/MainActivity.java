3ab7150f12e3743ce4c4aa3b18e6979d0c7ffe14
jaimesilvalorca <jaimesilvalorca@gmail.com>
2023-12-08 14:28:52 -0300

    trabajo listo

--- app/src/main/java/com/example/auto/MainActivity.java
+++ app/src/main/java/com/example/auto/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.auto;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText etPatente,etMarca,etModelo,etCilindrada;
 
     private ArrayList<Auto> listaAuto = new ArrayList<>();
+    private int posicionActual = -1;
 
 
     @Override
@@ -33,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
         btnGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 String patente, marca, modelo, cilindrada;
 
                 patente = etPatente.getText().toString();
@@ -43,11 +45,112 @@ public class MainActivity extends AppCompatActivity {
 
                 Auto auto = new Auto(patente, marca, modelo, cilindrada);
 
-                Log.d("TAG", "Datos ---->" + auto.getPatente() +" "+ auto.getMarca() + " "+ auto.getModelo()+ " " + auto.getCilindrada());
+                if (posicionActual >= 0 && posicionActual < listaAuto.size()) {
+                    listaAuto.set(posicionActual, auto);
+                } else {
+                    listaAuto.add(auto);
+                }
 
-
+                limpiarEditText();
+                
+                actualizarEstadoBotonGuardar();
             }
         });
+
+
+
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (posicionActual >= 0 && posicionActual < listaAuto.size()) {
+                    mostrarDialogoEliminar();
+                } else {
+                    Toast.makeText(MainActivity.this, "Ningún auto seleccionado para eliminar", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!listaAuto.isEmpty()) {
+                    posicionActual = (posicionActual + 1) % listaAuto.size();
+                    mostrarDatosEnEditText();
+                } else {
+                    Toast.makeText(MainActivity.this, "La lista está vacía", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!listaAuto.isEmpty()) {
+                    posicionActual = (posicionActual + 1) % listaAuto.size();
+                    mostrarDatosEnEditText();
+                } else {
+                    Toast.makeText(MainActivity.this, "La lista está vacía", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!listaAuto.isEmpty()) {
+                    // Si llegamos al inicio, ir al final; de lo contrario, retroceder una posición
+                    posicionActual = (posicionActual - 1 + listaAuto.size()) % listaAuto.size();
+                    mostrarDatosEnEditText();
+                } else {
+                    Toast.makeText(MainActivity.this, "La lista está vacía", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    private void mostrarDialogoEliminar() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Eliminar Auto");
+        builder.setMessage("¿Estás seguro de que deseas eliminar este auto?");
+        builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Eliminar el objeto en la posición seleccionada
+                listaAuto.remove(posicionActual);
+                limpiarEditText();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // No hacer nada si se selecciona "No"
+            }
+        });
+
+        builder.create().show();
+    }
+
+    private void actualizarEstadoBotonGuardar() {
+        // Deshabilitar el botón "Guardar" cuando la lista tiene 4 elementos
+        btnGuardar.setEnabled(listaAuto.size() < 4);
+    }
+
+    private void limpiarEditText() {
+        etPatente.getText().clear();
+        etMarca.getText().clear();
+        etModelo.getText().clear();
+        etCilindrada.getText().clear();
+    }
+
+    private void mostrarDatosEnEditText() {
+        if (posicionActual >= 0 && posicionActual < listaAuto.size()) {
+            Auto autoSeleccionado = listaAuto.get(posicionActual);
+            etPatente.setText(autoSeleccionado.getPatente());
+            etMarca.setText(autoSeleccionado.getMarca());
+            etModelo.setText(autoSeleccionado.getModelo());
+            etCilindrada.setText(autoSeleccionado.getCilindrada());
+        }
     }
 
     public void referencias(){
